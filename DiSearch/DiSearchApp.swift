75c2d07a76fd5f8a68aa5919219f2bff4611afd9
Hugo Dario Luna Cruz <hugo.luna@webpoint.mx>
2021-10-21 20:43:54 -0500

    Initial Commit

---
+++ DiSearch/DiSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiSearchApp.swift
+//  DiSearch
+//
+//  Created by Hugo Luna on 21/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DiSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
